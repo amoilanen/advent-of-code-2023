bfbbb1113b9d2441ac3d7520298f0b903fd5ee7b
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-02-08 16:02:22 +0300

    Solution for part 1 day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -7,8 +7,19 @@ pub fn parse(input: &str) -> Vec<&str> {
     input.split_terminator('\n').collect()
 }
 
+pub fn line_to_number(line: &str) -> u32 {
+    let mut digits = line.chars().filter(|ch| ch.is_ascii_digit()).peekable();
+    let first = digits.next();
+    let last = digits.last().or_else(|| first);
+    let number: u32 = vec![first, last].iter().flatten().fold(0, |acc, ch| acc * 10 + (*ch as u32 - '0' as u32) );
+    number
+}
+
 pub fn solution_part_1(parsed_input: &Vec<&str>) -> u32 {
-    return 1;
+    let numbers = parsed_input.iter().map(|line|
+        line_to_number(line)
+    );
+    numbers.sum()
 }
 
 pub fn solution_part_2(parsed_input: &Vec<&str>) -> u32 {
--- src/days/day1_tests.rs
+++ src/days/day1_tests.rs
@@ -2,7 +2,7 @@
 use crate::days::day1;
 
 #[test]
-fn parse_input() {
+fn test_parse_input() {
     let input: &str = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
@@ -14,4 +14,30 @@ treb7uchet";
       "a1b2c3d4e5f",
       "treb7uchet"
     ].to_vec())
+}
+
+#[test]
+fn test_line_to_number_multiple_digits() {
+  assert_eq!(day1::line_to_number("a1b2c3d4e5f"), 15)
+}
+
+#[test]
+fn test_line_to_number_exactly_two_digits() {
+  assert_eq!(day1::line_to_number("pqr3stu8vwx"), 38)
+}
+
+#[test]
+fn test_line_to_number_a_single_digit() {
+  assert_eq!(day1::line_to_number("treb7uchet"), 77)
+}
+
+#[test]
+fn test_line_to_number_no_digits() {
+  assert_eq!(day1::line_to_number("abc"), 0)
+}
+
+#[test]
+fn test_solution_part1() {
+  let parsed = day1::parse(day1::INPUT);
+  assert_eq!(day1::solution_part_1(&parsed), 142)
 }
