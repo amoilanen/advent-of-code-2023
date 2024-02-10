3032275cf2a54a0b3dc7eb6290a53d387194da86
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-02-11 01:55:20 +0300

    Make the first parsing test pass

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub const INPUT_PART: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
@@ -12,6 +14,14 @@ pub struct CubeSet {
     pub blue: u16
 }
 
+impl CubeSet {
+    pub fn new(red: u16, green: u16, blue: u16) -> Self {
+        CubeSet {
+            red, green, blue
+        }
+    }
+}
+
 #[derive(PartialEq)]
 #[derive(Debug)]
 pub struct Game {
@@ -19,11 +29,44 @@ pub struct Game {
     pub drawings: Vec<CubeSet>
 }
 
+impl Game {
+    pub fn new(id: u16, drawings: Vec<CubeSet>) -> Self {
+        Game { id, drawings }
+    }
+}
+
+fn parse_drawing(input: &str) -> CubeSet {
+    fn find_color_count(cube_counts: &Vec<(u16, &str)>, color_to_find: &str) -> u16 {
+        cube_counts.iter().find_map(|(count, color)|
+            if *color == color_to_find { Some(*count) } else { None }
+        ).unwrap_or(0)
+    }
+    let cube_counts: Vec<(u16, &str)> = input.split(",").map(|count_input| {
+        let parts: Vec<&str> = count_input.trim().split(' ').take(2).collect::<Vec<&str>>();
+        let (count, color) = (u16::from_str_radix(parts[0], 10).unwrap(), parts[1]);
+        (count, color)
+    }).collect();
+    let red_count = find_color_count(&cube_counts, "red");
+    let green_count = find_color_count(&cube_counts, "green");
+    let blue_count = find_color_count(&cube_counts, "blue");
+    CubeSet::new(red_count, green_count, blue_count)
+}
+
+fn parse_line(line: &str) -> Game {
+    let split_line = line.split(':').take(2).collect::<Vec<&str>>();
+    let (game_input, drawings_input) = (split_line[0], split_line[1]);
+    let game_id = u16::from_str_radix(&game_input["Game ".len()..], 10).unwrap();
+    let drawings: Vec<CubeSet> = drawings_input.split(';').map(|drawing_input| parse_drawing(drawing_input)).collect();
+    Game {
+        id: game_id,
+        drawings: drawings
+    }
+}
+
 pub fn parse(input: &str) -> Vec<Game> {
-    vec![Game {
-        id: 1,
-        drawings: Vec::new()
-    }]
+    input.split_terminator('\n')
+        .map(|line| parse_line(line))
+        .collect()
 }
 
 pub fn solution_part_1(parsed_input: &Vec<Game>) -> u16 {
--- src/days/day2_tests.rs
+++ src/days/day2_tests.rs
@@ -1,23 +1,20 @@
 #[cfg(test)]
 use crate::days::day2;
-
+#[cfg(test)]
 use crate::days::day2::Game;
+#[cfg(test)]
 use crate::days::day2::CubeSet;
 
 #[test]
 fn test_parse_single_line_input() {
     let input: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
     let result = day2::parse(input);
-    //TODO: Create an utility method to create games and cubesets in a shorter way
     assert_eq!(result, vec![
-        Game {
-            id: 1,
-            drawings: vec![
-                CubeSet { red: 4, green: 0, blue: 3 },
-                CubeSet { red: 1, green: 2, blue: 6 },
-                CubeSet { red: 0, green: 2, blue: 0 }
-            ]
-        }
+        Game:: new(1, vec![
+            CubeSet::new(4, 0, 3),
+            CubeSet::new(1, 2, 6),
+            CubeSet::new(0, 2, 0)
+        ])
     ])
 }
 
