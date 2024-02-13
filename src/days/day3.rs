4520e7acd6e7522a23e70fa82a2633633b3f5154
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-02-14 00:08:09 +0300

    Parse input for Day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -9,21 +9,27 @@ pub const INPUT: &str = "467..114..
 ...$.*....
 .664.598..";
 
-
-fn parse_line(line: &str) -> &str {
-    "todo"
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub struct Schematic {
+    pub rows: Vec<Vec<char>>
 }
 
-pub fn parse(input: &str) -> Vec<&str> {
-    input.split_terminator('\n')
+fn parse_line(line: &str) -> Vec<char> {
+    line.chars().collect()
+}
+
+pub fn parse(input: &str) -> Schematic {
+    let rows = input.split_terminator('\n')
         .map(|line| parse_line(line.trim()))
-        .collect()
+        .collect();
+    Schematic { rows }
 }
 
-pub fn solution_part_1(parsed_input: &Vec<&str>) -> u16 {
+pub fn solution_part_1(parsed_input: &Schematic) -> u16 {
     1
 }
 
-pub fn solution_part_2(parsed_input: &Vec<&str>) -> u32 {
+pub fn solution_part_2(parsed_input: &Schematic) -> u32 {
     1
 }
--- src/days/day3_tests.rs
+++ src/days/day3_tests.rs
@@ -1,3 +1,4 @@
+use crate::day3::Schematic;
 #[cfg(test)]
 use crate::days::day3;
 
@@ -6,5 +7,8 @@ fn test_parse() {
     let input: &str = "12..34
 ....*.";
     let result = day3::parse(input);
-    assert_eq!(result, vec!["abc"])
+    assert_eq!(result, day3::Schematic { rows: vec![
+        "12..34".chars().collect(),
+        "....*.".chars().collect()
+    ] })
 }
