1789417c395c119aef7fa8e4a88054c76dac7edf
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-02-14 01:59:55 +0300

    Parse numbers and symbols for Day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 pub const INPUT: &str = "467..114..
 ...*......
 ..35..633.
@@ -11,8 +13,55 @@ pub const INPUT: &str = "467..114..
 
 #[derive(PartialEq)]
 #[derive(Debug)]
+pub struct Position {
+    pub row: usize,
+    pub column: usize
+}
+
+impl Position {
+    fn is_adjacent_to(&self, other: &Position) -> bool {
+        ((other.row as i16) - (self.row as i16)).abs() <= 1
+        &&
+        ((other.column as i16) - (self.column as i16)).abs() <= 1
+    }
+
+    pub fn new(row: usize, column: usize) -> Position {
+        Position {row, column}
+    }
+}
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub struct SchematicNumber {
+    pub value: u16,
+    pub coordinates: Vec<Position>
+}
+
+impl SchematicNumber {
+    pub fn new(value: u16, coordinates: Vec<Position>) -> SchematicNumber {
+        SchematicNumber {value, coordinates}
+    }
+}
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub struct SchematicSymbol {
+    pub value: char,
+    pub position: Position
+}
+
+impl SchematicSymbol {
+    pub fn new(value: char, position: Position) -> SchematicSymbol {
+        SchematicSymbol {value, position}
+    }
+}
+
+#[derive(PartialEq)]
+#[derive(Debug)]
 pub struct Schematic {
-    pub rows: Vec<Vec<char>>
+    pub rows: Vec<Vec<char>>,
+    pub numbers: Vec<SchematicNumber>,
+    pub symbols: Vec<SchematicSymbol>
 }
 
 fn parse_line(line: &str) -> Vec<char> {
@@ -20,10 +69,52 @@ fn parse_line(line: &str) -> Vec<char> {
 }
 
 pub fn parse(input: &str) -> Schematic {
-    let rows = input.split_terminator('\n')
+    let rows: Vec<Vec<char>> = input.split_terminator('\n')
         .map(|line| parse_line(line.trim()))
         .collect();
-    Schematic { rows }
+
+
+    fn on_number_read(current_char: char, current_number: &mut Vec<char>) {
+        current_number.push(current_char)
+    }
+
+    fn on_number_read_finished(row_index: usize, column_index: usize, current_number: &mut Vec<char>, numbers: &mut Vec<SchematicNumber>) {
+        let number: String = current_number.iter().collect::<String>();
+        let coordinates: Vec<Position> = (column_index - number.len()..column_index).map(|column| Position::new(row_index, column)).collect();
+        numbers.push(SchematicNumber::new(number.parse().unwrap_or(0), coordinates));
+        current_number.clear()
+    }
+
+    fn on_symbol_read_finished(current_char: char, row_index: usize, column_index: usize, symbols: &mut Vec<SchematicSymbol>) {
+        symbols.push(SchematicSymbol::new(current_char, Position::new(row_index, column_index)))
+    }
+
+    let mut numbers: Vec<SchematicNumber> = Vec::new();
+    let mut symbols: Vec<SchematicSymbol> = Vec::new();
+    for (row_index, row) in rows.iter().enumerate() {
+        let number_of_columns = row.len();
+        let mut column_index = 0;
+        let mut current_number: Vec<char> = Vec::new();
+        while column_index < number_of_columns {
+            let current_char = row[column_index];
+            if current_char.is_numeric() {
+                on_number_read(current_char, &mut current_number);
+            } else {
+                // Non-numeric current_char
+                if current_number.len() > 0 {
+                    on_number_read_finished(row_index, column_index, &mut current_number, &mut numbers);
+                }
+                if current_char != '.' {
+                    on_symbol_read_finished(current_char, row_index, column_index, &mut symbols);
+                }
+            }
+            column_index = column_index + 1;
+        }
+        if current_number.len() > 0 {
+            on_number_read_finished(row_index, column_index, &mut current_number, &mut numbers);
+        }
+    }
+    Schematic { rows, numbers, symbols }
 }
 
 pub fn solution_part_1(parsed_input: &Schematic) -> u16 {
--- src/days/day3_tests.rs
+++ src/days/day3_tests.rs
@@ -1,14 +1,23 @@
-use crate::day3::Schematic;
+use crate::day3::{Position, Schematic, SchematicNumber, SchematicSymbol};
 #[cfg(test)]
 use crate::days::day3;
 
 #[test]
 fn test_parse() {
-    let input: &str = "12..34
-....*.";
+    let input: &str = "12..*.
+..34..";
     let result = day3::parse(input);
-    assert_eq!(result, day3::Schematic { rows: vec![
-        "12..34".chars().collect(),
-        "....*.".chars().collect()
-    ] })
+    assert_eq!(result, day3::Schematic {
+        rows: vec![
+            "12..*.".chars().collect(),
+            "..34..".chars().collect()
+        ],
+        numbers: vec![
+            SchematicNumber::new(12, vec![ Position::new(0, 0), Position::new(0, 1)]),
+            SchematicNumber::new(34, vec![ Position::new(1, 2), Position::new(1, 3)]),
+        ],
+        symbols: vec![
+            SchematicSymbol::new('*', Position::new(0, 4))
+        ]
+    })
 }
