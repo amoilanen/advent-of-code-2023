504e91994730ac8ac10ae534c900b8bdbc65fe69
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-03-03 18:04:28 +0300

    Parsing for Day 5

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,3 +1,7 @@
+use regex::Regex;
+use core::str::FromStr;
+use std::fmt::Debug;
+
 pub const INPUT: &str = "seeds: 79 14 55 13
 
 seed-to-soil map:
@@ -71,14 +75,48 @@ impl Almanac {
     }
 }
 
+fn parse_numbers<T>(numbers_input: &str) -> Vec<T> where T: FromStr,<T as FromStr>::Err: Debug {
+    let whitespace_regex = Regex::new(r"\s+").unwrap();
+    whitespace_regex.split(numbers_input)
+        .filter(|numbers_input| !numbers_input.is_empty())
+        .map(|number_input|
+            number_input.parse().expect("Expected a number")
+        ).collect()
+}
+
 pub fn parse(input: &str) -> Almanac {
     let lines: Vec<&str> = input.split_terminator('\n')
         .map(|line| line.trim())
         .collect();
-    //TODO: Implement
+    
+    let (_, inputs_input) = lines[0].split_once(':').expect("Did not find a single separator :");
+    let inputs: Vec<u32> = parse_numbers(inputs_input);
+
+    let mut maps: Vec<Map> = Vec::new();
+    
+    let mut rest_of_lines = lines.iter().skip(2);
+    let mut current_rules: Vec<RangeRule> = Vec::new();
+    while let Some(line) = rest_of_lines.next() {
+        if line.trim().is_empty() {
+            let map = Map::new(current_rules);
+            maps.push(map);
+            current_rules = Vec::new();
+        } else if line.find(':').is_some() {
+            // Skip the mapping header
+        } else {
+            let line_numbers: Vec<u32> = parse_numbers(line);
+            let (destination_start, source_start, range_length) = (line_numbers[0], line_numbers[1], line_numbers[2]);
+            current_rules.push(RangeRule::new(destination_start, source_start, range_length));
+        }
+    }
+    if !current_rules.is_empty() {
+        let map = Map::new(current_rules);
+        maps.push(map);
+        current_rules = Vec::new();
+    }
     Almanac {
-        inputs: Vec::new(),
-        maps: Vec::new()
+        inputs,
+        maps
     }
 }
 
