5051824fbc3749922ed4613ed2ab3ea7f6a15d4e
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-03-10 01:30:06 +0300

    Minor

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::fmt::Debug;
 use std::cmp::min;
 use std::cmp::max;
--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,11 @@ fn main() {
     let day_3_input_parsed = day3::parse(day3::INPUT);
     println!("{:?}", day3::solution_part_1(&day_3_input_parsed));
     println!("{:?}", day3::solution_part_2(&day_3_input_parsed));
+    println!("Day 4:");
     let day_4_input_parsed = day4::parse(day4::INPUT);
     println!("{:?}", day4::solution_part_1(&day_4_input_parsed));
     println!("{:?}", day4::solution_part_2(&day_4_input_parsed));
+    println!("Day 5:");
     let day_5_part_1_input_parsed = day5::parse_for_part_1(day5::INPUT);
     println!("{:?}", day5::solution_part_1(&day_5_part_1_input_parsed));
     let day_5_part_2_input_parsed = day5::parse_for_part_2(day5::INPUT);
