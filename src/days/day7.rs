26388a55560fb45822fc53fc54d2ed592c0057e4
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-03-16 19:43:39 +0300

    Implement parsing of the input for Day 7

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
 use crate::days::parsing;
 
 pub const INPUT: &str = "32T3K 765
@@ -6,8 +9,12 @@ KK677 28
 KTJJT 220
 QQQJA 483";
 
+#[derive(Eq)]
+#[derive(Hash)]
 #[derive(PartialEq)]
 #[derive(Debug)]
+#[derive(Clone)]
+#[derive(Copy)]
 pub struct Card {
     pub value: char
 }
@@ -95,9 +102,53 @@ impl Bid {
     }
 }
 
+fn counts<T>(elements: &Vec<T>) -> HashMap<T, u16>
+where T: Eq, T: Hash, T: Copy {
+    let mut counts = HashMap::new();
+    for &element in elements.iter() {
+        let count = counts.entry(element).or_insert(0);
+        *count += 1;
+    }
+    counts
+}
+
+fn determine_hand_type(cards: &[Card; 5]) -> HandType {
+    let card_counts_hash = counts(&cards.to_vec());
+    let card_counts: Vec<&u16> = card_counts_hash.values().collect();
+    let count_counts = counts(&card_counts);
+    let five_counts = *count_counts.get(&5).unwrap_or(&0);
+    let four_counts = *count_counts.get(&4).unwrap_or(&0);
+    let three_counts = *count_counts.get(&3).unwrap_or(&0);
+    let two_counts = *count_counts.get(&2).unwrap_or(&0);
+    if five_counts == 1 {
+        HandType::FiveOfAKind
+    } else if four_counts == 1 {
+        HandType::FourOfAKind
+    } else if three_counts == 1 && two_counts == 1 {
+        HandType::FullHouse
+    } else if three_counts == 1 {
+        HandType::ThreeOfAKind
+    } else if two_counts == 2 {
+        HandType::TwoPair
+    } else if two_counts == 1 {
+        HandType::OnePair
+    } else {
+        HandType::HighCard
+    }
+}
+
+fn parse_line(line: &str) -> Bid {
+    let (cards_input, bid_input) = line.split_once(' ').expect("Did not find a single separator :");
+    let bid_amount: u16 = bid_input.trim().parse().unwrap();
+    let cards: Vec<Card> = cards_input.chars().map(|card| Card::new(card)).collect();
+    let hand_cards: [Card; 5] = cards.try_into().unwrap();
+    let hand_type = determine_hand_type(&hand_cards);
+    let hand: Hand = Hand::new(hand_cards, hand_type);
+    Bid::new(hand, bid_amount)
+}
+
 pub fn parse(input: &str) -> Vec<Bid> {
-    //TODO: Implement
-    Vec::new()
+    parsing::as_lines(input).iter().map(|line| parse_line(line.trim())).collect()
 }
 
 pub fn solution_part_1(input: &Vec<Bid>) -> u64 {
